c11c5a8b5c354522e6b46c0ea3b06d52dc95d62e
Nikita Keaton <69308011+wtf-keaton@users.noreply.github.com>
2024-09-08 00:24:00 +0300

    Improved validate token method

--- prism-auth-service/go.mod
+++ prism-auth-service/go.mod
@@ -4,6 +4,7 @@ go 1.22.5
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1 // indirect
 	github.com/gofiber/fiber/v2 v2.52.5 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
--- prism-auth-service/internal/router/token/token.go
+++ prism-auth-service/internal/router/token/token.go
@@ -1,11 +1,51 @@
 package token
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Claims struct {
+	jwt.StandardClaims
+
+	Username string `json:"username"`
+}
 
 func ValidateToken(c *fiber.Ctx) error {
-	return c.SendString("test")
+	tokenData := c.FormValue("t")
+
+	jwtToken, err := jwt.ParseWithClaims(tokenData, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+
+		return nil, nil
+	})
+
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"Status": "failed",
+			"msg":    "Unexpected signing method",
+		})
+	}
+
+	if claims, ok := jwtToken.Claims.(*Claims); ok && jwtToken.Valid {
+		return c.JSON(fiber.Map{
+			"Status": "success",
+			"msg":    "Token valid",
+			"data":   claims.Username,
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"Status": "failed",
+		"msg":    "Failed to validate token",
+	})
 }
 
 func RefreshToken(c *fiber.Ctx) error {
+
 	return c.SendString("test2")
 }
